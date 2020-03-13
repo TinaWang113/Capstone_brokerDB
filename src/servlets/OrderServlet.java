0bf9eedea17af0466f26a83c24068e139a955496
Nathan Fairbairn <nfairbairn@live.com>
2020-03-13 02:59:36 -0600

    [MenuAndOrder]Removed some old files

--- src/servlets/OrderServlet.java
+++ src/servlets/OrderServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class Order
  */
 
-public class Order extends HttpServlet {
+public class OrderServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
